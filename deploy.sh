a6ebf25a3ce0a4e9b804555cda2ecf75852475c9
27 <zhengchengzhong@yuewen.com>
2021-05-26 09:42:40 +0800

    docs: update docs to gh-pages

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,20 @@
 # deploy website
 set -e
 
+# 提交 gh-pages
 yarn build
-
 cd build
 git init
 git co -b gh-pages
 git add -A
 git commit -m 'docs: update docs to gh-pages'
 
-git push -f git@github.com:s-acss/sacss.git gh-pages
+git push -f git@github.com:s-acss/s-acss.github.io.git gh-pages
+
+# 提交 当前代码
+cd ..
+git add -A
+git commit -m 'docs: update docs to gh-pages'
+git push
 
 cd -
